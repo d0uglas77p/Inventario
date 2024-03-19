9b251a879193cd5908f066f4fcafaa68aae5d8f6
d0uglas77p <douglas_1313@hotmail.com>
2024-03-19 20:52:49 -0300

    inv-math01 - Testando branch Math

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Lincoln");
+        System.out.println("Math");
 
         // Cria tabelas de teste do banco de dados
         new ConexaoBD().criarTabelaTeste();
