79a1b7f3e355e93847de3c3d3fad2216f09c939d
d0uglas77p <douglas_1313@hotmail.com>
2024-03-19 20:41:48 -0300

    inv-zank01 - Testando Branch

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,14 @@
 package application;
 
+import application.model.conexao.ConexaoBD;
+
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("TESTE");
+
+        // Cria tabelas de teste do banco de dados
+        new ConexaoBD().criarTabelaTeste();
     }
 }
