e3ff2ca40a6cab26636634cfe4fd15902ca3ab4e
d0uglas77p <douglas_1313@hotmail.com>
2024-03-19 20:49:36 -0300

    inv-lincoln01 - testando branch Lincoln

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("TESTE");
+        System.out.println("Lincoln");
 
         // Cria tabelas de teste do banco de dados
         new ConexaoBD().criarTabelaTeste();
